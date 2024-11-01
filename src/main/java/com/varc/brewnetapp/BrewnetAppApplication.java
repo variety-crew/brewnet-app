966a0a8733af0b2e707dc5ee25b5636d3feb677e
Yong-ga-ri <rlfgks97@gmail.com>
2024-11-01 16:43:44 +0900

    init

---
+++ src/main/java/com/varc/brewnetapp/BrewnetAppApplication.java
@@ -0,0 +1,13 @@
+package com.varc.brewnetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewnetAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrewnetAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/varc/brewnetapp/BrewnetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varc.brewnetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewnetAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
