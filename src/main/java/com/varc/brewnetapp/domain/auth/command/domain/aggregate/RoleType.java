42ea7d8507732714a74f1dd5960cd5469c0a3580
gilhan <rlfgks97@gmail.com>
2024-11-04 00:29:16 +0900

    fix: update Role type

--- src/main/java/com/varc/brewnetapp/domain/auth/command/domain/aggregate/RoleType.java
+++ src/main/java/com/varc/brewnetapp/domain/auth/command/domain/aggregate/RoleType.java
@@ -6,10 +6,10 @@ import lombok.Getter;
 public enum RoleType {
       MASTER("ROLE_MASTER", 1)
     , ADMIN("ROLE_ADMIN", 2)
-    , COMPANY_HEAD("COMPANY_HEAD", 3)
-    , COMPANY_STAFF("COMPANY_STAFF", 4)
-    , FRANCHISE("FRANCHISE", 5)
-    , DRIVER("DRIVER", 6);
+    , COMPANY_HEAD("ROLE_COMPANY_HEAD", 3)
+    , COMPANY_STAFF("ROLE_COMPANY_STAFF", 4)
+    , FRANCHISE("ROLE_FRANCHISE", 5)
+    , DRIVER("ROLE_DRIVER", 6);
 
     private final String type;
     private final int roleId;
