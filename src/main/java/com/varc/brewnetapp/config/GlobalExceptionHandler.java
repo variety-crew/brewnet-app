cf24002f2c3f3da6f959bfff9fd471317a7994cf
rohdamin <rohdamin3@gmail.com>
2024-11-17 18:50:05 +0900

    feat: ExchangeNotFoundException, InvalidStatusException 추가

--- src/main/java/com/varc/brewnetapp/config/GlobalExceptionHandler.java
+++ src/main/java/com/varc/brewnetapp/config/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.varc.brewnetapp.config;
 
 import com.varc.brewnetapp.common.ResponseMessage;
 import com.varc.brewnetapp.exception.DuplicateException;
+import com.varc.brewnetapp.exception.ExchangeNotFoundException;
+import com.varc.brewnetapp.exception.InvalidStatusException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +16,21 @@ public class GlobalExceptionHandler {
 
     // 400: 잘못된 요청 예외 처리
     @ExceptionHandler({
-        DuplicateException.class
+        DuplicateException.class,
+        InvalidStatusException.class
     })
     public ResponseEntity<ResponseMessage<Object>> handleBadRequestException(Exception e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
             .body(new ResponseMessage<>(400, e.getMessage(), null));
+    }
+
+    // 404 Not Found
+    @ExceptionHandler({
+        ExchangeNotFoundException.class
+    })
+    public ResponseEntity<ResponseMessage<Object>> handleNotFoundException(Exception e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(new ResponseMessage<>(404, e.getMessage(), null));
     }
 }
 
