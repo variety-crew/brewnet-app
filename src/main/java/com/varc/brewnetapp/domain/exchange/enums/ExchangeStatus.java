cc29b8b84b3bd0d5cdba0516630a63dcf4bc30b5
rohdamin <rohdamin3@gmail.com>
2024-11-17 18:50:35 +0900

    fix: 교환상태 enum 수정

--- src/main/java/com/varc/brewnetapp/domain/exchange/enums/ExchangeStatus.java
+++ src/main/java/com/varc/brewnetapp/domain/exchange/enums/ExchangeStatus.java
@@ -2,14 +2,14 @@ package com.varc.brewnetapp.domain.exchange.enums;
 
 public enum ExchangeStatus {
     REQUESTED("교환요청"),
+    CANCELED("교환취소"),
     APPROVED("교환승인"),
+    REJECTED("교환반려"),
     PICKING("수거중"),
     PICKED("수거완료"),
     SHIPPING("배송중"),
     SHIPPED("배송완료"),
-    COMPLETED("교환완료"),
-    CANCEL("교환취소"),
-    RETURN("교환반려");
+    COMPLETED("교환완료");
 
     private String krName;
 
