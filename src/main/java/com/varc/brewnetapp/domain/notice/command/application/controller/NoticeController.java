da9c9c0df650687de4026559c7441cc4b986da8e
JeongJunSeo99 <wnstj0810@naver.com>
2024-11-22 10:08:01 +0900

    chore: notice domain 개발을 위한 기본 설정

---
+++ src/main/java/com/varc/brewnetapp/domain/notice/command/application/controller/NoticeController.java
@@ -0,0 +1,8 @@
+package com.varc.brewnetapp.domain.notice.command.application.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "commandNoticeController")
+public class NoticeController {
+
+}
---
+++ src/main/java/com/varc/brewnetapp/domain/notice/command/application/service/NoticeServiceImpl.java
@@ -0,0 +1,8 @@
+package com.varc.brewnetapp.domain.notice.command.application.service;
+
+import org.springframework.stereotype.Service;
+
+@Service(value = "commandNoticeService")
+public class NoticeServiceImpl {
+
+}
