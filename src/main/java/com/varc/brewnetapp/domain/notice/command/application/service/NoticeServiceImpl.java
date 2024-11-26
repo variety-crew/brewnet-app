c61e0d1a5bd327a105180f0b942afffa2080879b
JeongJunSeo99 <wnstj0810@naver.com>
2024-11-26 09:51:02 +0900

    fix: 아이템 활성화 된 것만 가져오기

--- src/main/java/com/varc/brewnetapp/domain/notice/command/application/service/NoticeServiceImpl.java
+++ src/main/java/com/varc/brewnetapp/domain/notice/command/application/service/NoticeServiceImpl.java
@@ -3,6 +3,6 @@ package com.varc.brewnetapp.domain.notice.command.application.service;
 import org.springframework.stereotype.Service;
 
 @Service(value = "commandNoticeService")
-public class NoticeServiceImpl {
+public class NoticeServiceImpl implements NoticeService {
 
 }
