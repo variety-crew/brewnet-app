96161cf87ea9d49d909c3e939dca22a966e77085
gilhan <rlfgks97@gmail.com>
2024-11-30 11:58:29 +0900

    refactor: Pagenagtion -> List

--- src/main/java/com/varc/brewnetapp/domain/item/query/controller/ItemOrderController.java
+++ src/main/java/com/varc/brewnetapp/domain/item/query/controller/ItemOrderController.java
@@ -3,13 +3,9 @@ package com.varc.brewnetapp.domain.item.query.controller;
 import com.varc.brewnetapp.common.ResponseMessage;
 import com.varc.brewnetapp.domain.item.query.dto.MustBuyItemDTO;
 import com.varc.brewnetapp.domain.item.query.service.ItemService;
-import com.varc.brewnetapp.domain.item.query.service.ItemServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,20 +24,11 @@ public class ItemOrderController {
 
     @GetMapping
     @Operation(summary = "필수 구매 품목 리스트 조회")
-    public ResponseEntity<ResponseMessage<Page<MustBuyItemDTO>>> getMustBuyItems(
-            @PageableDefault(size = 10, page = 0) Pageable pageable,
-            @RequestParam(name = "filter", required = false) String filter,
-            @RequestParam(name = "sort", required = false) String sort
-    ) {
-        Page<MustBuyItemDTO> mustBuyItemList = queryItemService.getMustBuyItemsBy(
-                pageable,
-                filter,
-                sort
-        );
+    public ResponseEntity<ResponseMessage<List<MustBuyItemDTO>>> getMustBuyItems() {
 
         return ResponseEntity.ok(
                 new ResponseMessage<>(
-                        200, "OK", mustBuyItemList
+                        200, "OK", queryItemService.getMustBuyItemsBy()
                 )
         );
     }
