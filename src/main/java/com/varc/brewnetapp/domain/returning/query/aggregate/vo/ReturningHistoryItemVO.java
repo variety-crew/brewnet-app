616dfa98a56d20a74ed2918a0e74f8cd710c8cb4
rohdamin <rohdamin3@gmail.com>
2024-12-04 15:00:01 +0900

    hotfix: ReturningHistoryItemVO 수정

--- src/main/java/com/varc/brewnetapp/domain/returning/query/aggregate/vo/ReturningHistoryItemVO.java
+++ src/main/java/com/varc/brewnetapp/domain/returning/query/aggregate/vo/ReturningHistoryItemVO.java
@@ -3,10 +3,12 @@ package com.varc.brewnetapp.domain.returning.query.aggregate.vo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class ReturningHistoryItemVO {
     private String itemUniqueCode;  // 상품 고유 코드
     private String itemName;        // 상품명
