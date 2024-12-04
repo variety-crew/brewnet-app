6c3858cf7d747ec3846b38a683c3cc69e67945e1
rohdamin <rohdamin3@gmail.com>
2024-12-04 11:34:36 +0900

    hotfix: 교환/반품 타부서 상세조회 시 교환/반품코드 포함

--- src/main/java/com/varc/brewnetapp/domain/returning/query/aggregate/vo/ReturningHistoryItemVO.java
+++ src/main/java/com/varc/brewnetapp/domain/returning/query/aggregate/vo/ReturningHistoryItemVO.java
@@ -1,5 +1,12 @@
 package com.varc.brewnetapp.domain.returning.query.aggregate.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class ReturningHistoryItemVO {
     private String itemUniqueCode;  // 상품 고유 코드
     private String itemName;        // 상품명
