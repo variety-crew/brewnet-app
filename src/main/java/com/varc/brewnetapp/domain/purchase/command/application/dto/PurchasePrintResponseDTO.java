7b59ab1beb00be0245cd9b7a33842e1d10eeee86
hoddeok07 <jhj2969@naver.com>
2024-12-07 22:21:06 +0900

    fix: 잘못 import한 타입 수정

--- src/main/java/com/varc/brewnetapp/domain/purchase/command/application/dto/PurchasePrintResponseDTO.java
+++ src/main/java/com/varc/brewnetapp/domain/purchase/command/application/dto/PurchasePrintResponseDTO.java
@@ -15,7 +15,7 @@ public class PurchasePrintResponseDTO {
     private int letterOfPurchaseCode;               // 발주서 코드
     private String createdAt;                       // 발주서 생성일시
     private String memberName;                      // 발주서 작성자(기안자)명
-    private PositionName positionName;              // 작성자 직급
+    private PositionName positionName;              // 작성자 직위
     private String companyName;                     // 본사명
     private String businessNumber;                  // 본사 사업자등록번호
     private String corporateNumber;                 // 본사 법인등록번호
