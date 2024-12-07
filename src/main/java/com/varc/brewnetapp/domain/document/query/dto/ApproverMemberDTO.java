d86d55791bec9b2208163c6f4026cedf1d5b366f
hoddeok07 <jhj2969@naver.com>
2024-12-07 22:08:16 +0900

    feat: 발주 결재 요청/승인/반려 알림 기능 추가 refactor: 쓸데없는 repository, entity 삭제

--- src/main/java/com/varc/brewnetapp/domain/document/query/dto/ApproverMemberDTO.java
+++ src/main/java/com/varc/brewnetapp/domain/document/query/dto/ApproverMemberDTO.java
@@ -1,6 +1,6 @@
 package com.varc.brewnetapp.domain.document.query.dto;
 
-import com.varc.brewnetapp.domain.purchase.common.PositionNameEx;
+import com.varc.brewnetapp.domain.member.command.domain.aggregate.entity.Position;
 import lombok.*;
 
 @NoArgsConstructor
@@ -12,5 +12,5 @@ public class ApproverMemberDTO {
 
     private int approverCode;               // 결재자 회원코드
     private String approverName;            // 결재자명
-    private PositionNameEx positionName;    // 직급명
+    private Position positionName;          // 직급명
 }
--- src/main/java/com/varc/brewnetapp/domain/purchase/command/application/dto/PurchasePrintResponseDTO.java
+++ src/main/java/com/varc/brewnetapp/domain/purchase/command/application/dto/PurchasePrintResponseDTO.java
@@ -1,6 +1,6 @@
 package com.varc.brewnetapp.domain.purchase.command.application.dto;
 
-import com.varc.brewnetapp.domain.purchase.common.PositionNameEx;
+import com.varc.brewnetapp.domain.member.command.domain.aggregate.PositionName;
 import lombok.*;
 
 import java.util.List;
@@ -15,7 +15,7 @@ public class PurchasePrintResponseDTO {
     private int letterOfPurchaseCode;               // 발주서 코드
     private String createdAt;                       // 발주서 생성일시
     private String memberName;                      // 발주서 작성자(기안자)명
-    private PositionNameEx positionName;            // 작성자 직급
+    private PositionName positionName;              // 작성자 직급
     private String companyName;                     // 본사명
     private String businessNumber;                  // 본사 사업자등록번호
     private String corporateNumber;                 // 본사 법인등록번호
