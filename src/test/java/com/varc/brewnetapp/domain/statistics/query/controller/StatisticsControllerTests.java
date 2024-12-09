56458fc6c4cfd7a0fd47c3e7750805d949f98775
JeongJunSeo99 <wnstj0810@naver.com>
2024-12-09 11:26:41 +0900

    fix: 알람 message type String으로 고정

--- src/test/java/com/varc/brewnetapp/domain/statistics/query/controller/StatisticsControllerTests.java
+++ src/test/java/com/varc/brewnetapp/domain/statistics/query/controller/StatisticsControllerTests.java
@@ -1,6 +1,7 @@
 //package com.varc.brewnetapp.domain.statistics.query.controller;
 //
 //import static org.awaitility.Awaitility.given;
+//import static org.mockito.ArgumentMatchers.any;
 //
 //import com.varc.brewnetapp.domain.statistics.query.dto.NewOrderDTO;
 //import com.varc.brewnetapp.domain.statistics.query.service.StatisticsService;
@@ -9,20 +10,29 @@
 //import java.util.List;
 //import org.junit.jupiter.api.DisplayName;
 //import org.junit.jupiter.api.Test;
+//import org.mockito.BDDMockito;
 //import org.mockito.Mockito;
 //import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 //import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.boot.test.mock.mockito.MockBean;
 //import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
 //import org.springframework.data.domain.PageImpl;
 //import org.springframework.data.domain.PageRequest;
-//import org.springframework.data.domain.Pageable;
 //import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
-//import org.springframework.data.web.PageableDefault;
+//import org.springframework.http.MediaType;
+//import org.springframework.security.test.context.support.WithMockUser;
 //import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.ResultActions;
 //
-//@WebMvcTest(StatisticsController.class)
-//@MockBean(JpaMetamodelMappingContext.class)
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@AutoConfigureMockMvc
+//@SpringBootTest
 //class StatisticsControllerTests {
 //
 //    @Autowired
@@ -31,50 +41,34 @@
 //    @MockBean
 //    private StatisticsService statisticsService;
 //
+//
 //    @Test
 //    @DisplayName("신규 주문 목록 조회 로직 테스트")
+//    @WithMockUser(username = "wnstj", roles = "MASTER") // Mock 인증 추가
 //    public void findNewOrderTest() throws Exception {
 //
-//        // Given
-//        MemberDto.Post post = new MemberDto.Post("hgd@gmail.com", "홍길동", "010-1234-5678");
-//        String content = gson.toJson(post);
-//
-//        MemberDto.response responseDto =
-//            new MemberDto.response(1L,
-//                "hgd@gmail.com",
-//                "홍길동",
-//                "010-1234-5678",
-//                Member.MemberStatus.MEMBER_ACTIVE,
-//                new Stamp());
-//
+//        //given
 //        Pageable page = PageRequest.of(0, 10);
 //        List<NewOrderDTO> newOrderList = new ArrayList<>();
 //        newOrderList.add(new NewOrderDTO(1, 1, "테스트 프랜차이즈", "고구마", 1000, "2024-12-12"));
-//        Page<NewOrderDTO> orderPage = new PageImpl<>(newOrderList, page, 1);
 //
+//        Page<NewOrderDTO> orderPage = new PageImpl<>(newOrderList, page, newOrderList.size());
 //
-//        given(statisticsService.findNewOrder(Mockito.any(Pageable.class))).willReturn(orderPage);
-//        given(mapper.memberPostToMember(Mockito.any(MemberDto.Post.class))).willReturn(new Member());
-//
-//        given(memberService.createMember(Mockito.any(Member.class))).willReturn(new Member());
-//
-//        given(mapper.memberToMemberResponse(Mockito.any(Member.class))).willReturn(responseDto);
+//        Mockito.when(statisticsService.findNewOrder(page)).thenReturn(orderPage);
 //
 //        // When
 //        ResultActions actions =
-//            mockMvc.perform(
-//                post("/members")
-//                    .accept(MediaType.APPLICATION_JSON)
-//                    .contentType(MediaType.APPLICATION_JSON)
-//                    .content(content)
+//            mockMvc.perform(get("/api/v1/hq/statistics/newOrder")
+//                .param("page", "0") // 페이지 번호
+//                .param("size", "10") // 페이지 크기
+//                .accept(MediaType.APPLICATION_JSON)
 //            );
 //
 //        // Then
 //        actions
-//            .andExpect(status().isCreated())
-//            .andExpect(jsonPath("$.data.email").value(post.getEmail()))
-//            .andExpect(jsonPath("$.data.name").value(post.getName()))
-//            .andExpect(jsonPath("$.data.phone").value(post.getPhone()));
+//            .andExpect(status().isOk())
+//            .andExpect(jsonPath("$.result.content[0].franchiseName").value(newOrderList.get(0).getFranchiseName()));
 //    }
+//
 //
 //}
